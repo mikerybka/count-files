c02fbf870ad72ddca2007137b36fb167837c86f1
Mike Rybka <merybka@gmail.com>
2025-04-09 10:58:21 -0400

    Create count-files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mikerybka/count-files
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	root := "."
+	if len(os.Args) > 2 {
+		root = os.Args[1]
+	}
+
+	count := 0
+	err := filepath.WalkDir(root, func(path string, d os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if !d.IsDir() {
+			count++
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		os.Exit(1)
+	}
+	fmt.Println(count)
+}
